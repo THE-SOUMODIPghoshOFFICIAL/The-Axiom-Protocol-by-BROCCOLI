49ed43facda9e680ea73a2683104eebdfebc8db9
THE-SOUMODIPghoshOFFICIAL <soumorim30@gmail.com>
2025-11-03 22:05:13 -0800

    add program statements

--- 01_the_Basics/temperature.c
+++ 01_the_Basics/temperature.c
@@ -1,5 +1,7 @@
 /**************************************************************************
-
+PROBLEM STATEMENT:- Temperature of a city in Fahrenheit degrees is input through the 
+keyboard. Write a program to convert this temperature into 
+Centigrade degrees. 
 **********************************************************************************/
 
 #include <stdio.h>
