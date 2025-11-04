9f979727e79a2719ccc3213cb2efbd5f590c1e79
THE-SOUMODIPghoshOFFICIAL <soumorim30@gmail.com>
2025-11-03 22:04:21 -0800

    changing in temp file

--- 01_the_Basics/temperature.c
+++ 01_the_Basics/temperature.c
@@ -1,7 +1,5 @@
 /**************************************************************************
-PROBLEM STATEMENT:- Temperature of a city in Fahrenheit degrees is input through the 
-keyboard. Write a program to convert this temperature into 
-Centigrade degrees. 
+
 **********************************************************************************/
 
 #include <stdio.h>
