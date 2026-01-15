2e5a78a8d292e220ea3d2aa7c8b4ed65ee745230
THE-SOUMODIPghoshOFFICIAL <soumorim30@gmail.com>
2026-01-14 22:51:51 -0800

    create new problem with its problem statement and theory

---
+++ Logic Building Arsenal/CATEGORY 1_NUMBER THEORY & MATHEMATICS/1.1Basic-Number-Properties/PosNegZe.c
@@ -0,0 +1,24 @@
+
+/* PROBLEM #2: Check if Number is POSITIVE, NEGATIVE, or ZERO
+===================================================================
+Mathematical Concept
+Numbers can be divided into 3 categories based on their value:
+1. POSITIVE Numbers (> 0)
+
+Greater than zero
+Examples: 1, 2, 5, 100, 9999
+On number line: RIGHT side of zero
+
+2. NEGATIVE Numbers (< 0)
+
+Less than zero
+Examples: -1, -5, -100, -9999
+On number line: LEFT side of zero
+
+3. ZERO (= 0)
+
+Neither positive nor negative
+The middle point
+Only ONE number in this category!*/
+
+
