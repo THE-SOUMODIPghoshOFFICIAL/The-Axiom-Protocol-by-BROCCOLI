4b9e83cf0641c7f530cdec6a313aa10b4f31e37e
THE-SOUMODIPghoshOFFICIAL <soumorim30@gmail.com>
2026-01-16 18:14:53 -0800

    add problem 3 with its problem documentations

---
+++ Logic Building Arsenal/CATEGORY 1_NUMBER THEORY & MATHEMATICS/1.1Basic-Number-Properties/3SwapeNum.c
@@ -0,0 +1,48 @@
+/*PROBLEM #3: SWAP Two Numbers (With & Without Temp Variable)
+=============================================================================
+##What Does "SWAP" Mean?
+-------------------------
+Swapping means exchanging the values of two variables.
+Real-Life Example:
+Imagine two glasses:
+
+Glass A has Orange Juice 
+Glass B has Apple Juice 
+
+Goal: Exchange their contents!
+After swapping:
+
+Glass A has Apple Juice 
+Glass B has Orange Juice 
+===============================================================================
+##The Problem:-
+---------------
+Before Swap:  |   After Swap:
+   a = 10     |     a = 20
+   b = 20     |     b = 10
+
+==============================================================================
+##Common MISTAKE (This Won't Work!):------
+
+a = b;  // Now a = 20, b = 20
+b = a;  // Now a = 20, b = 20
+
+
+#What went wrong?
+-------------------
+
+Step 1: a = b
+        a = 20, b = 20  ❌ (Original value of a is LOST!)
+
+Step 2: b = a
+        a = 20, b = 20  ❌ (Both have same value!)
+
+
+The original value of a (which was 10) is gone forever!
+
+
+
+
+
+
+*/
