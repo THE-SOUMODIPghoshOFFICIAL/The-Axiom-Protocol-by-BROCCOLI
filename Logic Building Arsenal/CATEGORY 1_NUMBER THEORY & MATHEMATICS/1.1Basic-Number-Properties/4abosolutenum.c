4436231913003694229d58b25e47131b8b57a973
THE-SOUMODIPghoshOFFICIAL <soumorim30@gmail.com>
2026-01-25 22:50:24 -0800

    documentation 1

--- Logic Building Arsenal/CATEGORY 1_NUMBER THEORY & MATHEMATICS/1.1Basic-Number-Properties/4abosolutenum.c
+++ Logic Building Arsenal/CATEGORY 1_NUMBER THEORY & MATHEMATICS/1.1Basic-Number-Properties/4abosolutenum.c
@@ -1,14 +1,5 @@
  /*PROBLEM #4: Find ABSOLUTE VALUE of a Number
 =====================================================================
-  ##What is ABSOLUTE VALUE?
-Absolute value = Distance from zero (always POSITIVE or ZERO)
-Simple Definition:
-
-Remove the negative sign (if any)
-Keep positive numbers as they are
-
-Symbol:   Math: |x|
-Example: |-5| = 5, |5| = 5
  
 ==========================================================================
  ##Visual Understanding on Number Line:-
