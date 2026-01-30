9d1f1e72a9704c066ef0184a14d6ab66428d2462
THE-SOUMODIPghoshOFFICIAL <soumorim30@gmail.com>
2026-01-29 22:12:59 -0800

    add documentation in the program

--- Logic Building Arsenal/CATEGORY 1_NUMBER THEORY & MATHEMATICS/1.1Basic-Number-Properties/4abosolutenum.c
+++ Logic Building Arsenal/CATEGORY 1_NUMBER THEORY & MATHEMATICS/1.1Basic-Number-Properties/4abosolutenum.c
@@ -13,13 +13,15 @@ Example: |-5| = 5, |5| = 5
 ==========================================================================
  ##Visual Understanding on Number Line:-
 
-      ←─────────────|─────────────→
+              ←─────────────|─────────────→
         -5  -4  -3  -2  -1  0  1  2  3  4  5
     
    |-5| = 5  (distance from 0 is 5 units)
-   |5|  = 5  (distance from 0 is 5 units)
+   |5|  = 5  (distance from 0 is 5 units)    [ Key Point: Distance is ALWAYS positive!]
    |0|  = 0  (distance from 0 is 0 units)
- 
+========================================================================================================= 
+
+
 
  */
 
