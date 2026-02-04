79a19dcd8c81ca0091145ccb224a00322385021d
THE-SOUMODIPghoshOFFICIAL <soumorim30@gmail.com>
2026-02-04 23:51:09 +0530

    initializing new problem on divisibility

---
+++ Logic Building Arsenal/CATEGORY 1_NUMBER THEORY & MATHEMATICS/1.1Basic-Number-Properties/5divisibilityOFnumbers.c
@@ -0,0 +1,15 @@
+/* PROBLEM #5: Check if a Number is DIVISIBLE by Another
+==================================================================================================================
+
+  ##What is DIVISIBILITY?
+----------------------------
+
+Divisibility means one number can be divided by another with NO REMAINDER.
+Simple Definition:
+
+Number A is divisible by Number B if:
+
+A ÷ B gives a whole number (no decimal)
+Remainder is ZERO
+
+*/
