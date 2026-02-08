635263e2976ca5f04951a15e60e992faf679807a
THE-SOUMODIPghoshOFFICIAL <soumorim30@gmail.com>
2026-02-08 23:48:56 +0530

    undo undo operation

--- Logic Building Arsenal/CATEGORY 1_NUMBER THEORY & MATHEMATICS/1.1Basic-Number-Properties/5divisibilityOFnumbers.c
+++ Logic Building Arsenal/CATEGORY 1_NUMBER THEORY & MATHEMATICS/1.1Basic-Number-Properties/5divisibilityOFnumbers.c
@@ -63,10 +63,13 @@ int main() {
     printf("Enter the divisor: ");
     scanf("%d", &divisor);
 
+    printf("\n--- CHECKING DIVISIBILITY ---\n");
+    printf("Number: %d\n", number);
+    printf("Divisor: %d\n", divisor);
 
 
 
 
 
-
+    
 }
