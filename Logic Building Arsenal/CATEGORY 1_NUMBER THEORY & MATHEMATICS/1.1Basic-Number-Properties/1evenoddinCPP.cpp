205e1cfa77a682719800473327ce6ab164de376a
THE-SOUMODIPghoshOFFICIAL <soumorim30@gmail.com>
2026-05-20 21:08:48 +0530

    problem evenodd in c++

--- Logic Building Arsenal/CATEGORY 1_NUMBER THEORY & MATHEMATICS/1.1Basic-Number-Properties/1evenoddinCPP.cpp
+++ Logic Building Arsenal/CATEGORY 1_NUMBER THEORY & MATHEMATICS/1.1Basic-Number-Properties/1evenoddinCPP.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+using namespace std;
+ int main()
+ {  
+    int a;
 
-int main() {
-    std::cout << "Hiii jiku\n";
+    cout << "Enter any number : " ;
+    cin  >>a;
+
+    if (a%2==0)
+    {
+        cout << "EVEN NUMBER !" ; 
+    }
+    
+    else
+    {
+         cout << "ODD NUMBER !" ;
+    }
+    
     return 0;
-}
+ }
+ 
