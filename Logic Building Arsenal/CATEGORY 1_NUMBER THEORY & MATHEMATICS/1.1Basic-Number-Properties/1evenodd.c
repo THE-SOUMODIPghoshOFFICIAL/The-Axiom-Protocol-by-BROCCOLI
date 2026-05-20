60a33b58282ad582e992b88ae36390a8d9273b2d
THE-SOUMODIPghoshOFFICIAL <soumorim30@gmail.com>
2026-05-20 09:38:30 +0530

    fresh start after removing 100mp file

--- Logic Building Arsenal/CATEGORY 1_NUMBER THEORY & MATHEMATICS/1.1Basic-Number-Properties/1evenodd.c
+++ Logic Building Arsenal/CATEGORY 1_NUMBER THEORY & MATHEMATICS/1.1Basic-Number-Properties/1evenodd.c
@@ -7,7 +7,8 @@
 Even number: Any number that can be divided by 2 without remainder
 Examples: 2, 4, 6, 8, 10, 100, 1000
 Odd number: Any number that gives remainder 1 when divided by 2
-Examples: 1, 3, 5, 7, 9, 99, 101*
+Examples: 1, 3, 5, 7, 9, 99, 101
+
 
 The Magic Formula: number % 2
 If number % 2 == 0 → Even
